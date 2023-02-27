fe501c30e2ffa22af0f7d68fc4863a40f2ef4b77
logahuff688 <113370990+logahuff688@users.noreply.github.com>
2023-02-27 06:23:09 -0500

    updates

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Get all the draggable icons
+const icons = document.querySelectorAll(".audioIcon");
+
+// Get all the title elements
+const titles = document.querySelectorAll(".title");
+
+// Define the sound effect to be played
+const soundEffect = new Audio("path/to/sound/effect.mp3");
+
+// Add event listeners to each icon
+icons.forEach(icon => {
+  // When the drag starts, store the ID of the dragged icon
+  icon.addEventListener("dragstart", event => {
+    event.dataTransfer.setData("text/plain", event.target.id);
+  });
+});
+
+// Add event listeners to each title
+titles.forEach(title => {
+  // When an icon is dragged over a title, prevent the default behavior
+  title.addEventListener("dragover", event => {
+    event.preventDefault();
+  });
+
+  // When an icon is dropped on a title, play the sound effect
+  title.addEventListener("drop", event => {
+    event.preventDefault();
+
+    // Get the ID of the dropped icon
+    const iconId = event.dataTransfer.getData("text/plain");
+
+    // Play the sound effect
+    soundEffect.play();
+
+    console.log(`Icon ${iconId} dropped on ${title.innerText}`);
+  });
+});
+
