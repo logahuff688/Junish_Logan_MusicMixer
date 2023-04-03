e82fd7026e6f03f96aabcff78eaf2e196165b743
Jerryjerry18 <113370950+Jerryjerry18@users.noreply.github.com>
2023-04-03 15:33:55 -0400

    All done
    Happy with project all files are working

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Get all the draggable icons
-const icons = document.querySelectorAll("#icon");
-const Gear1 = document.querySelector('#icon');
-const Gear2 = document.querySelector('#icon2');
-const Gear3 = document.querySelector('#icon3');
-const Gear4 = document.querySelector('#icon4');
-const Gear5 = document.querySelector('#icon5');
-const Gear6 = document.querySelector('#icon6');
+const icons = document.querySelectorAll(".container img");
+const Gear1 = document.querySelector('#icon3');
+const Gear2 = document.querySelector('#icon4');
+const Gear3 = document.querySelector('#icon5');
+const Gear4 = document.querySelector('#icon6');
+const Gear5 = document.querySelector('#icon1');
+const Gear6 = document.querySelector('#icon2');
 
 // Get all the title elements
 const boxes = document.querySelectorAll(".DragDropBox");
@@ -14,17 +14,19 @@ const Box = document.querySelector('#Box');
 
 // Define the sound effects to be played
 const screwdriverSound = new Audio("assets/screwdriver.mp3");
-const hammerSound = new Audio("assets/hammer.mp3");
+const hammerSound = new Audio("assets/Hammer.mp3");
 const wrenchSound = new Audio("assets/wrench.mp3");
 const wirecutterSound = new Audio("assets/wirecutters.mp3");
+const disksound = new Audio("assets/disk.mp3");
+const metalsound = new Audio("assets/metal.mp3");
 
 // Add event listeners to each icon
 icons.forEach(icon => {
   // When the drag starts, store the ID of the dragged icon
   icon.addEventListener("dragstart", event => {
-    event.dataTransfer.setData("text/plain", event.target.id);
-    
-    
+    //debugger;
+    event.dataTransfer.setData("target", event.target.id);
+
   });
 });
 
@@ -40,27 +42,23 @@ boxes.forEach(box => {
     event.preventDefault();
 
     // Get the ID of the dropped icon
-    const iconId = event.dataTransfer.getData("text/plain");
-
+    const iconId = event.dataTransfer.getData("target");
+    debugger;
     // Play the sound effect based on the element that was dropped on
-    if (Gear1 === event.target) {
+    if (Gear1.id === iconId) {
       hammerSound.play();
-    } else if (Gear2 === event.target) {
+    } else if (Gear2.id === iconId) {
       wrenchSound.play();
-    } else if (Box === event.target) {
+    } else if (Gear6.id === iconId) {
       screwdriverSound.play();
-    } else if (Gear3 === event.target) {
+    } else if (Gear3.id === iconId) {
       wirecutterSound.play();
-    }
-
- else if (Gear4 === event.target) {
-  hammerSound.play();
+    } else if (Gear4.id === iconId) {
+      disksound.play();
+    } else if (Gear5.id === iconId) {
+      metalsound.play();
 }
 
-else if (Gear5 === event.target) {
-  hammerSound.play();
-}
-
-    console.log(`Icon ${iconId} dropped on ${box.innerText}`);
+    console.log(`Icon ${iconId} dropped on ${Box.innerText}`);
   });
 });
